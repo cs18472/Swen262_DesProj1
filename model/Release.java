06ffba8e6b157b1727b9b0b1c94b41ba29bb58b1
Colby Scarbrough <cws18472@gmail.com>
2022-02-10 12:31:29 -0500

    complete static block library for model

--- model/Release.java
+++ model/Release.java
@@ -3,16 +3,21 @@ package model;
 import java.util.List;
 
 public class Release {
+    private String guid;
     private String title;
     private String artistGuid;
     private String artist;
+    private String issueDate;
+    private String medium;
     private List<Song> songs;
     private int rating;
 
-    public Release (String title, String artistGuid, String artist, List<Song> songs) {
+    public Release (String guid,String title, String artistGuid, String artist,String issueDate, String medium, List<Song> songs) {
         this.title = title;
         this.artistGuid = artistGuid;
         this.artist = artist;
+        this.issueDate = issueDate;
+        this.medium = medium;
         this.songs = songs;
         
         int totalNum = 0;
@@ -22,8 +27,22 @@ public class Release {
             totalRating += s.getRating();
         }
 
-        this.rating = totalNum/totalRating;
+        if(totalNum == 0 || totalRating == 0) this.rating = 0;
+        else this.rating = totalRating/totalNum;
 
+    }
+    public void addArtist(String name) {
+        this.artist = name;
+    }
+
+    public String getGuid() {
+        return guid;
+    }
+    public String getMedium() {
+        return medium;
+    }
+    public String getIssueDate() {
+        return issueDate;
     }
     public String getArtist() {
         return artist;
