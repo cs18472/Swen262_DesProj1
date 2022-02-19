54dc09cfbddab39379776df2b5d95dd3f5f051ff
Colby Scarbrough <cws18472@gmail.com>
2022-02-19 16:29:13 -0500

    small changes

--- view/CLI.java
+++ view/CLI.java
@@ -8,18 +8,12 @@ public class CLI{
 
     public CLI() {
         JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
 
         frame.setSize(200, 200);
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Muze Music Library");
 
-        panel.setLayout(null);
-        JLabel title = new JLabel();
-        title.setLayout(null);
-        title.setText("Muze Music Library");
-        panel.add(title);
         
         JTextField commandLine = new JTextField();
         commandLine.setVisible(true);
@@ -27,7 +21,6 @@ public class CLI{
         commandLine.setSize(800, 50);
 
         frame.add(commandLine, BorderLayout.SOUTH);
-        frame.add(panel, BorderLayout.NORTH);
 
         frame.setVisible(true);
     }
