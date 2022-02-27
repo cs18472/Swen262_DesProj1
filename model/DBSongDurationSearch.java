20edb5d1dfab290b547aa4138e9a925dee09ebb3
mason <mgk7525@rit.edu>
2022-02-27 18:14:11 -0500

    Added files for the needed searches (Not Implemented, Check Trello)

---
+++ model/DBSongDurationSearch.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.List;
+
+public class DBSongDurationSearch implements DatabaseSearcher{
+
+    @Override
+    public List<LibraryElement> doSearch(List<LibraryElement> elements, String input) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
