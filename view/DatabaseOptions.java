6453f4213da3ac1ee0b167f16ce83160816c9c9b
VrangrGitHub <lukesp@outlook.com>
2022-03-11 16:24:59 -0500

    did I forget to push

--- view/DatabaseOptions.java
+++ view/DatabaseOptions.java
@@ -10,7 +10,13 @@ public class DatabaseOptions implements Page {
 
     @Override
     public void menu() {
-        // TODO Auto-generated method stub
+        System.out.println("------------------------------------------------");
+        System.out.println("Enter '1' to search for a song.");
+        System.out.println("Enter '2' to search for an artist.");
+        System.out.println("Enter '3' to search for a release.");
+        System.out.println("Enter 'B' to return to the previous page.");
+        System.out.println("Enter 'Q' to return to the landing page.");
+        System.out.println("------------------------------------------------");
         
     }
 
@@ -26,7 +32,25 @@ public class DatabaseOptions implements Page {
 
     @Override
     public void interpretInput(String str) {
-        // TODO Auto-generated method stub
+        if((str).equals("1")){
+            System.out.println("Enter the song name.");
+            //call again
+            //databaseSearch(songName);
+        }
+        else if((str).equals("2")){
+            
+        }
+        else if((str).equals("q")){
+            quit();
+        }
+        
+        else{
+            System.out.println("------------------------------------------------");
+
+            System.out.println("ERROR: Please enter a valid menu option.");
+
+            System.out.println("------------------------------------------------");
+        }
     }
     
 }
