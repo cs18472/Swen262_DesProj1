ad1f52542ae13b7e9a22ac5e9c0f0efb8f6751b4
VrangrGitHub <lukesp@outlook.com>
2022-03-12 18:41:29 -0500

    merge conflict

--- view/ScannerCLI.java
+++ view/ScannerCLI.java
@@ -4,13 +4,5 @@ import java.util.Scanner;
 
 public class ScannerCLI {
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Welcome to the Muze Music Library System \nPlease Press Enter to continue");
-        try{        System.in.read();}catch(Exception e){	e.printStackTrace();}
-        System.out.println("Where would you like to go? Press the corresponding number key to choose your option.");
-        System.out.println("1. View Personal Library\n 2.Edit Personal Libary \n ");
-
-
     }
 }
