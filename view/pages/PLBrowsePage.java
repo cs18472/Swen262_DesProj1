0c3164ecbb59604ca1e9933775f9de652de3b446
VrangrGitHub <lukesp@outlook.com>
2022-03-14 22:07:45 -0400

    small changes

---
+++ view/pages/PLBrowsePage.java
@@ -0,0 +1,44 @@
+package view.pages;
+
+import view.ScannerCLI;
+
+
+public class PLBrowsePage extends Page {
+    private final ScannerCLI scanner;
+
+    public PLBrowsePage(ScannerCLI scanner){
+        this.scanner = scanner;
+    }
+
+    @Override
+    public void menu() {
+        this.interpretInput("1");
+        System.out.println("------------------------------------------------");
+        System.out.println("Please enter '1' to explore an artist.");
+        System.out.println("Please enter '2' to explore an .");
+        System.out.println("Enter 'B' to return to the previous page.");
+        System.out.println("Enter 'Q' to return to the landing page.");
+        System.out.println("------------------------------------------------");
+        
+    }
+
+    @Override
+    public void interpretInput(String str) {
+        if (checkQuit(str)){
+            if((str).equals("1")){
+                //display all Artists
+            }
+
+        }
+    }
+
+    @Override
+    public void quit() {
+        scanner.setPage(new LandingPage(scanner));
+    }
+
+    @Override
+    public void back() {
+        scanner.setPage(new PLSearchDirectoryPage(scanner));
+    }
+}
